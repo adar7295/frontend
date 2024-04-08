dca12e79a01db022541c8a60ccd488fde17af23f
adar7295 <ssrivastav7295@gmail.com>
2024-04-08 15:31:48 +0530

    bootstrap use

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import classes from './login.module.css'
 
 const Login = () => {
   return (
+    <div>
     <h1>Login</h1>
+    
+
+   <button className={classes.loginBtn}>Login Button</button>
+   <button className='loginBtn'>Login Button</button>
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h1>Adarsh Srivastav</h1>
+        <h1 style={{ textAlign:'center', fontSize:50 }}>Welcome to Home Page...</h1>
+
+        <img src ="/next.svg" alt="" />
+        <hr/><br/><input type="text" />
+
+        <button className='mybtn'>My Button</button>
     </div>
   )
 }
