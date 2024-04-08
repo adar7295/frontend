9682d8d1435865705b4188959318be56fac5d459
adar7295 <ssrivastav7295@gmail.com>
2024-04-09 00:11:13 +0530

    loginpage

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,12 +3,15 @@ import classes from './login.module.css'
 
 const Login = () => {
   return (
-    <div>
-    <h1>Login</h1>
-    
-
-   <button className={classes.loginBtn}>Login Button</button>
-   <button className='loginBtn'>Login Button</button>
+    <div className={classes.backg}>
+        <div className={classes.main}>
+      <h2 className={classes.login}>Login-Page</h2>
+      Name<br/>
+      <input type="text" placeholder='Enter your name' /><br/>
+      Password<br />
+      <input type="password" placeholder='Enter your password' /><br/>
+      <button className='btn btn-primary'>Login</button>
+     </div>
     </div>
   )
 }
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <h1>Signup</h1>
+    <h2>Signup</h2>
   )
 }
 
