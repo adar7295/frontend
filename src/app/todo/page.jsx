bf55dacfaa61c103b64a35be20c96958af72a1b3
adar7295 <ssrivastav7295@gmail.com>
2024-04-15 15:01:25 +0530

    todolist

---
+++ src/app/todo/page.jsx
@@ -0,0 +1,46 @@
+'use client'
+import React, { useState } from 'react';
+
+const Todo = () => {
+
+     const [todolist, setTodolist] = useState([
+        { task:'Do homeWork',completed:false },
+        { task:'Eat',completed:false },
+        { task:'watch movies',completed:false },
+        { task:'play games',completed:false }
+     ]);     
+    const addtask=(e)=>{
+        if(e.code ==='Enter'){
+            console.log(e.target.value);
+            setTodolist([ ...todolist, { task : e.target.value, completed: false }]);
+            e.target.value='';
+
+        }
+
+    }
+
+
+  return (
+    <div className='container py-5'>
+     <h2 className='text-center'>Todo App</h2>
+     <hr />
+        <div className="card">
+            <div className="card-header">
+                <input onKeyDown={addtask} type="text" className='form-control border-primary border-2'/>
+            </div>
+            <div className="card-body">
+                {
+                    todolist.map( (item, index)=>{
+                        return <div className='d-flex justify-content-between p-3'>
+                            <p>{item.task}</p>
+                            <button onClick={ () => { deleteTask(index) } }className='btn btn-danger'>Delete</button>
+                        </div>
+                    } )
+                }
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Todo
