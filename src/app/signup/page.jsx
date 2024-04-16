10c8072621d1033460ee9ec8163b684c4f6636d1
adar7295 <ssrivastav7295@gmail.com>
2024-04-16 15:33:15 +0530

    validation

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,6 +2,14 @@
 import React from 'react'
 import classes from './signup.module.css'
 import { useFormik } from 'formik'
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  name : Yup.string().min(4,'Enter Full Name').required('Name required'),
+  email : Yup.string().email('Invalid email').required('Required'),
+  password : Yup.string().min(6, 'too short').required('Required'),
+
+});
 
 
 const Signup = () => {
@@ -17,7 +25,8 @@ const Signup = () => {
     },
     onSubmit : (values) => {
       console.log(values);//send values to backend
-    }
+    },
+    validationSchema : SignupSchema
 
   })
   return (
@@ -28,16 +37,25 @@ const Signup = () => {
       <form onSubmit={ signupForm.handleSubmit }>
 
       <label className={classes.mainlabel}>Name</label><br/>
+
       <input type="text" id='name' onChange={ signupForm.handleChange } value={signupForm.values.name}/>
-     
+       {signupForm.touched.name && (
+        <small className='text-danger'>{signupForm.errors.name}</small>
+       )}
       <label className={classes.mainlabel}>Email</label><br/>
       <input type="Email" id='email' onChange={ signupForm.handleChange } value={signupForm.values.email}/>
+      {signupForm.touched.email && (
+        <small className='text-danger'>{signupForm.errors.email}</small>
+       )}
       <label className={classes.mainlabel}>Password</label><br/>
-      <input type="password" id='password' onChange={ signupForm.handleChange } value={signupForm.values.password}/>
+      <input className='form-control' type="password" id='password' onChange={ signupForm.handleChange } value={signupForm.values.password}/>
+      {signupForm.touched.password && (
+        <small className='text-danger'>{signupForm.errors.password}</small>
+       )}
       <label className={classes.mainlabel}>Confirm Password</label><br/>
       <input type="password" id='confirmPassword' onChange={ signupForm.handleChange } value={signupForm.values.confirmPassword}/>
       
-      <button className={classes.btn}>Signup</button>
+      <button type='submit' className={classes.btn}>Signup</button>
       </form>
       </div>
     </div>
