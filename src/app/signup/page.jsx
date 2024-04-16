7a554814d5835a283d569fd936d35f941739d87a
adar7295 <ssrivastav7295@gmail.com>
2024-04-16 14:56:44 +0530

    forms

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,23 +1,44 @@
+'use client'
 import React from 'react'
 import classes from './signup.module.css'
+import { useFormik } from 'formik'
+
 
 const Signup = () => {
+
+  const signupForm = useFormik({
+    initialValues : {
+    name : '',
+    
+    email :'',
+    password : '',
+    confirmPassword : ''
+    
+    },
+    onSubmit : (values) => {
+      console.log(values);//send values to backend
+    }
+
+  })
   return (
     <div className={classes.backg}>
       <div className={classes.main}>
 
       <h2 className={classes.signup}>Signup</h2>
+      <form onSubmit={ signupForm.handleSubmit }>
+
       <label className={classes.mainlabel}>Name</label><br/>
-      <input type="text" />
-      <label className={classes.mainlabel}>Password</label><br/>
-      <input type="password" />
+      <input type="text" id='name' onChange={ signupForm.handleChange } value={signupForm.values.name}/>
+     
       <label className={classes.mainlabel}>Email</label><br/>
-      <input type="Email" />
-      <label className={classes.mainlabel}>Contact No.</label><br/>
-      <input type="number" />
-      <label className={classes.mainlabel}>Address</label><br/>
-      <input type="text" />
+      <input type="Email" id='email' onChange={ signupForm.handleChange } value={signupForm.values.email}/>
+      <label className={classes.mainlabel}>Password</label><br/>
+      <input type="password" id='password' onChange={ signupForm.handleChange } value={signupForm.values.password}/>
+      <label className={classes.mainlabel}>Confirm Password</label><br/>
+      <input type="password" id='confirmPassword' onChange={ signupForm.handleChange } value={signupForm.values.confirmPassword}/>
+      
       <button className={classes.btn}>Signup</button>
+      </form>
       </div>
     </div>
   )
