71e7ed4942d30fd5a2fcd31eecd9f4d0af1af464
adar7295 <ssrivastav7295@gmail.com>
2024-04-25 15:04:32 +0530

    signup

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,66 +1,173 @@
-'use client'
-import React from 'react'
-import classes from './signup.module.css'
-import { useFormik } from 'formik'
+
+'use client';
+import { useFormik } from 'formik';
+import Link from 'next/link';
+import React from 'react';
 import * as Yup from 'yup';
 
 const SignupSchema = Yup.object().shape({
-  name : Yup.string().min(4,'Enter Full Name').required('Name required'),
-  email : Yup.string().email('Invalid email').required('Required'),
-  password : Yup.string().min(6, 'too short').required('Required'),
-
+  name: Yup.string().min(4, 'Name pura likho').required('Naam nhi hai kya?'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Password is required')
+    .min(6, 'Too small').matches(/[a-z]/, 'must include lowercase letters')
+    .matches(/[A-Z]/, 'must include uppercase letters')
+    .matches(/[0-9]/, 'must include numbers')
+    .matches(/\W/, 'must include special charecter'),
+  confirmPassword: Yup.string().oneOf([Yup.ref('password'),null], 'Password must match')
+    .required('password is required')
 });
-
 
 const Signup = () => {
 
   const signupForm = useFormik({
-    initialValues : {
-    name : '',
-    
-    email :'',
-    password : '',
-    confirmPassword : ''
-    
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
     },
-    onSubmit : (values) => {
-      console.log(values);//send values to backend
+    onSubmit: (values) => {
+      console.log(values);
+      // send request to backend
+      // recieve request at backend
+      // process the request
+      // send response back to frontend
+      fetch("http://localhost:5000/user/add", {
+        method: 'POST',
+        body:  JSON.stringify(values),
+        headers: {
+          'Content-Type':'application/json'
+        }
+      })
     },
-    validationSchema : SignupSchema
-
+    validationSchema: SignupSchema
   })
+
   return (
-    <div className={classes.backg}>
-      <div className={classes.main}>
+    <section className="vh-100 bg-primary-subtle">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col">
+            <div className="card shadow my-4">
+              <div className="row g-0">
+                <div className="col-xl-6 d-none d-xl-block">
+                  <div style={{
+                    backgroundImage: `url('https://assets.materialup.com/uploads/7563d4bc-0ed9-4202-a86c-ac8dc46e73ef/preview.jpg')`,
+                    height: '100%',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat'
+                  }}>
 
-      <h2 className={classes.signup}>Signup</h2>
-      <form onSubmit={ signupForm.handleSubmit }>
+                  </div>
+                  {/* <img
+                    src="https://assets.materialup.com/uploads/7563d4bc-0ed9-4202-a86c-ac8dc46e73ef/preview.jpg"
+                    alt="Sample"
+                    className="img-fluid"
+                  /> */}
+                </div>
+                <div className="col-xl-6">
 
-      <label className={classes.mainlabel}>Name</label><br/>
+                  <div className="card-body p-md-5">
+                    <h3 className="mb-5 text-primary fw-bold">
+                      Registration Form
+                    </h3>
+                    <form onSubmit={signupForm.handleSubmit} >
 
-      <input type="text" id='name' onChange={ signupForm.handleChange } value={signupForm.values.name}/>
-       {signupForm.touched.name && (
-        <small className='text-danger'>{signupForm.errors.name}</small>
-       )}
-      <label className={classes.mainlabel}>Email</label><br/>
-      <input type="Email" id='email' onChange={ signupForm.handleChange } value={signupForm.values.email}/>
-      {signupForm.touched.email && (
-        <small className='text-danger'>{signupForm.errors.email}</small>
-       )}
-      <label className={classes.mainlabel}>Password</label><br/>
-      <input className='form-control' type="password" id='password' onChange={ signupForm.handleChange } value={signupForm.values.password}/>
-      {signupForm.touched.password && (
-        <small className='text-danger'>{signupForm.errors.password}</small>
-       )}
-      <label className={classes.mainlabel}>Confirm Password</label><br/>
-      <input type="password" id='confirmPassword' onChange={ signupForm.handleChange } value={signupForm.values.confirmPassword}/>
-      
-      <button type='submit' className={classes.btn}>Signup</button>
-      </form>
+                      <div class="mb-3">
+                        <label for="" class="form-label">Email Address</label>
+                        <input
+                          type="text"
+                          id="email"
+                          onChange={signupForm.handleChange}
+                          value={signupForm.values.email}
+                          class="form-control"
+                          placeholder=""
+                        />
+                        {signupForm.touched.email && (
+                          <small class="text-danger">{signupForm.errors.email}</small>
+                        )}
+                      </div>
+                      <div class="mb-3">
+                        <label for="" class="form-label">Name</label>
+                        <input
+                          type="text"
+                          id="name"
+                          onChange={signupForm.handleChange}
+                          value={signupForm.values.name}
+                          class="form-control"
+                          placeholder=""
+                        />
+                        {signupForm.touched.name && (
+                          <small class="text-danger">{signupForm.errors.name}</small>
+                        )}
+                      </div>
+                      <div class="mb-3">
+                        <label for="" class="form-label">Password</label>
+                        <input
+                          type="password"
+                          id="password"
+                          onChange={signupForm.handleChange}
+                          value={signupForm.values.password}
+                          class="form-control"
+                          placeholder=""
+                        />
+                        {signupForm.touched.password && (
+                          <small class="text-danger">{signupForm.errors.password}</small>
+                        )}
+                      </div>
+                      <div class="mb-3">
+                        <label for="" class="form-label">Confirm Password</label>
+                        <input
+                          type="password"
+                          id="confirmPassword"
+                          onChange={signupForm.handleChange}
+                          value={signupForm.values.confirmPassword}
+                          class="form-control"
+                          placeholder=""
+                        />
+                        {signupForm.touched.confirmPassword && (
+                          <small class="text-danger">{signupForm.errors.confirmPassword}</small>
+                        )}
+                      </div>
+                      <div className="form-check mb-4">
+                        <input
+                          className="form-check-input me-2"
+                          type="checkbox"
+                          defaultValue=""
+                          id="form2Example33"
+                          defaultChecked=""
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="form2Example33"
+                        >
+                          I Agree to Terms & Conditions
+                        </label>
+                      </div>
+                      <div className="d-flex justify-content-end pt-3">
+                        <button type="button" className="btn btn-light">
+                          Reset all
+                        </button>
+                        <button type="submit" className="btn btn-primary ms-2">
+                          Submit form
+                        </button>
+                      </div>
+                    </form>
+
+                    <p>Already Registered? <Link href='/login'>Login Here</Link></p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    </section>
 
+  )
 }
 
 export default Signup;
+
