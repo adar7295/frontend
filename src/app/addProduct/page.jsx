0bf14da32bb732086db638a351986ba99f760f8a
adar7295 <ssrivastav7295@gmail.com>
2024-06-30 13:19:11 +0530

    frontend

---
+++ src/app/addProduct/page.jsx
@@ -0,0 +1,94 @@
+'use client'
+import { useFormik } from 'formik'
+import React from 'react'
+import toast from 'react-hot-toast';
+
+const product = () => {
+
+
+  const productDetails = useFormik({
+    initialValues: {
+      name: '',
+      price: '',
+      category: '',
+      image: ''
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      fetch('http://localhost:5000/product/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      })
+        .then((response) => {
+          console.log(response.status);
+          if (response.status === 200) {
+            toast.success('Product added successfully')
+          }
+        }).catch((err) => {
+          console.log(err);
+          toast.error("Something went wrong")
+        });
+
+    },
+  })
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <h2 className="text-center mb-4">Add Product</h2>
+        <form onSubmit={productDetails.handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="productName" className="form-label">Product Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.name}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="productPrice" className="form-label">Product Price</label>
+            <input
+              type="number"
+              className="form-control"
+              id="price"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.price}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="productCategory" className="form-label">Product Category</label>
+            <input
+              type="text"
+              className="form-control"
+              id="category"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.category}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="productImage" className="form-label">Product Image</label>
+            <input
+              type="text"
+              className="form-control"
+              id="image"
+              onChange={productDetails.handleChange}
+              value={productDetails.values.image}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default product;
---
+++ src/app/manageProduct/page.jsx
@@ -0,0 +1,55 @@
+"use client"
+import React, { useState,useEffect } from 'react'
+
+const manageProduct = () => {
+    const [productList,setProductList]=useState([])
+     
+    const fetchProduct=async()=>{
+        const res=await fetch('http://localhost:5000/product/getAll',{
+            method:'GET',
+        })
+        console.log(res.status);
+        const data=await res.json();
+        setProductList(data);
+    }
+    useEffect(()=>{
+        fetchProduct();
+    },[])
+
+  return (
+     
+
+    <div className='container'>
+        <h3 className='text-center mt-4 mb-5'>Manage Product</h3>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Category</th>
+      <th scope="col">Image</th>
+    </tr>
+  </thead>
+  <tbody> 
+   {
+    productList.map((product)=>{
+        return(
+            <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.image}</td>
+            </tr>
+        )
+    })
+   }
+  </tbody>
+</table>
+      
+    </div>
+  )
+}
+
+export default manageProduct
