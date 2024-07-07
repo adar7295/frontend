4eb341e2b8ed62aea25adbd0e5f3ed6e5d9fac28
adar7295 <ssrivastav7295@gmail.com>
2024-07-07 13:28:09 +0530

    token

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -10,46 +10,60 @@ import toast from 'react-hot-toast';
 const Login = () => {
 
   const loginForm = useFormik({
-    initialValues:{
-      email:'',
-      password:''
+    initialValues: {
+      email: '',
+      password: ''
     },
-    onSubmit :(values) =>{
+    onSubmit: (values) => {
       console.log(values);
-      console.log(200);
+
+      fetch("http://localhost:5000/user/authenticate/", {
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            toast.success('Login Successfull')
+          } else {
+
+            toast.error('Login Failed')
+          }
+
+          return response.json();
+        })
+        .then((result) => {
+          console.log(result);
+          localStorage.setItem('token',result.token);
+          
+        })
+        .catch((err) => {
+          console.log(err);
+          toast.error('Login Failed');
+        });
     }
   })
-  // .then((response) => {
-  //   console.log(response.status);
-  //   if(response === 200){
-  //     toast.success("login success");
-  //   }
-  //   else{
-  //     console.log(response.status)
-  //     toast.error('login failed')
-  //   }
-  // }).catch((err) => {
-  //   console.log(err);
-  //   toast.error('login failed');
-  // });
 
-    return (
+
+  return (
     <div className={classes.backg}>
-        <form action="">
+      <form onSubmit={loginForm.handleSubmit}>
         <div className={classes.main}>
-          
-      <h2 className={classes.login}>Login-Page</h2>
-      <label className={classes.mainlabel}>Email</label><br/>
-      <input id='email' onChange={loginForm.handleChange} value={loginForm.values.email} type="email" placeholder='Enter your email' /><br/>
-      
-      <label className={classes.mainlabel}>Password</label><br />
-      <input id='password' onChange={loginForm.handleChange} value={loginForm.values.password} type="password" placeholder='Enter your password' /><br/>
-      <button className={classes.btn} type='submit'>Login</button><br/>
-      
-      <label className={classes.mainlabel}>create new account.<Link href="/signup">Signup</Link></label>
-  
-     </div>
-     </form>
+
+          <h2 className={classes.login}>Login-Page</h2>
+          <label className={classes.mainlabel}>Email</label><br />
+          <input id='email' onChange={loginForm.handleChange} value={loginForm.values.email} type="email" placeholder='Enter your email' /><br />
+
+          <label className={classes.mainlabel}>Password</label><br />
+          <input id='password' onChange={loginForm.handleChange} value={loginForm.values.password} type="password" placeholder='Enter your password' /><br />
+          <button className={classes.btn} type='submit'>Login</button><br />
+
+          <label className={classes.mainlabel}>create new account.<Link href="/signup">Signup</Link></label>
+
+        </div>
+      </form>
     </div>
   )
 }
