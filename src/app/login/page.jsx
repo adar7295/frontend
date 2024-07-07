6cbd75ef9f593ac5382ae1eb170dff2a4f8c3608
adar7295 <ssrivastav7295@gmail.com>
2024-07-07 13:29:58 +0530

    cookie

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -37,7 +37,7 @@ const Login = () => {
         .then((result) => {
           console.log(result);
           localStorage.setItem('token',result.token);
-          
+          document.cookie = `token=${result.token}`;
         })
         .catch((err) => {
           console.log(err);
